88acf1ffd6a62a78bf725ccb2f87e9825a1095e7
Marco <marco.deangelis09@gmail.com>
2022-03-10 13:59:26 +0000

    Compute the area metric between tabular or tensor dataseries.

--- areametric/areametric.py
+++ areametric/areametric.py
@@ -1,14 +1,17 @@
 '''
-    %---------------------------------------#
-    Created Oct 2020
-     Edited Mar 2022
+    : --------------------------------------- #
+    Created: Oct 2020
+    Edited:  Mar 2022
+    
     Marco De Angelis & Jaleena Sunny
-    github.com/marcodeangelis
-    University of Liverpool
+    
+    web: github.com/marcodeangelis
+    org:University of Liverpool
+    
     GNU General Public License v3.0
-    %---------------------------------------#
+    : --------------------------------------- #
 
-    Computes the area metric between two dataseries or of a mixture.
+    Computes the area metric between two dataseries or the area metric of a mixture.
     
     This version works with data sets of different sizes.
 
@@ -19,29 +22,43 @@ from __future__ import annotations
 
 import numpy
 
-from numpy import (ndarray, concatenate, linspace, diff, argmax, argmin)
+from numpy import (ndarray, concatenate, linspace, diff, argmax, argmin, arange, transpose, prod, empty)
 
 from .dataseries import (dataseries, mixture)
-from .methods import (ecdf, ecdf_p, inverse_quantile_function, quantile_function)
-
-# from areametric.dataset import dataset_parser, Dataset, ecdf_, ecdf, DATASET_TYPE, pseudoinverse
+from .methods import (ecdf, ecdf_p, inverse_quantile_function, quantile_function, is_compatible)
 
 
-def areaMe(x_: ndarray, y_: ndarray) -> ndarray: # inputs lists of doubles # numpy array are not currently supported.
+def areame_algorithm(x_: ndarray, y_: ndarray) -> float: # inputs lists of doubles # numpy array are not currently supported.
     '''
-    ∞ --------------------------- ∞
-    cre: Wed Oct 7 2020 
+    : --------------------------- ∞ 
+
+    cre: Wed Oct 7 2020
     edi: Tue Mar 8 2022
-    author: Marco De Angelis 
-    github.com/marcodeangelis 
-    University of Liverpool 
+
+    web: github.com/marcodeangelis 
+    org: University of Liverpool 
+
     MIT
-    ∞ --------------------------- ∞
+
+    : --------------------------- ∞
+
+    Computes the area metric between two dataseries of dimension 1. A dataseries of dimension 1 is a 1d-array.
 
     Works also on datasets of different sizes.
 
-    When datasets have same size the code is fastest.
+    When datasets have same size or one size is a multiple of the other the code is fastest.
 
+    Inputs
+    ------
+    x : ndarray
+        An array-like structure of single dimension, e.g. a list or a Numpy array.
+    y : ndarray
+        An array-like structure of single dimension, e.g. a list or a Numpy array.
+
+    Output
+    ------
+    output : float
+        The area metric value.
     '''
     x, y = dataseries(x_), dataseries(y_) 
     n1, n2 = len(x), len(y)
@@ -51,15 +68,139 @@ def areaMe(x_: ndarray, y_: ndarray) -> ndarray: # inputs lists of doubles # num
     if (n1==n2) | ((n1>n2) & (n1%n2==0)): p = ecdf_p(x)
     elif (n2>n1) & (n2%n1==0): p = ecdf_p(y)
     else: # datasets of different size 
-        xy = x + y # concatenate the two dataseries
-        xysv = xy.value_sorted 
-        u = abs(iqx(xysv) - iqy(xysv))
-        v = diff(xysv)
-        return sum(u[:-1]*v)
-    p_= concatenate(([0.],p))
+        xy = x + y # concatenate the two dataseries # sort happens here again
+        xysv = xy.value_sorted # get data after sorting
+        u = abs(iqx(xysv) - iqy(xysv)) # steps height
+        v = diff(xysv) # steps width
+        return sum(u[:-1]*v) # area chunks
+    p_= concatenate(([0.],p)) 
     pm = (p+p_[:-1])/2 # mid height of each step
     return sum(abs(qx(pm) - qy(pm)) / n)
         
+def areame_tensor(x:ndarray,y:ndarray) -> ndarray: # x and y must have the same dimension (not shape!)
+    """
+    Inputs
+    ------
+    x : ndarray
+        An array-like structure of any dimension, e.g. a list or a Numpy array.
+    y : ndarray
+        An array-like structure of any dimension, e.g. a list or a Numpy array.
+
+    Output
+    ------
+    areame_sorted_reshape : ndarray
+        An ndarray containing the values of the area metric arranged in an array of shape equal to the dimension of x and y.
+    """
+    x_shape, y_shape = x.shape, y.shape
+    rep_x, rep_y = x_shape[0], y_shape[0]
+    dim_xy = x_shape[1:] # must be = y_shape[:1]
+    permut_x, permut_y = list(arange(len(x_shape))), list(arange(len(y_shape))) # order of dimension
+    permutx_rep_last, permuty_rep_last = permut_x[1:]+[permut_x[0]], permut_y[1:]+[permut_y[0]] # new order of dimensions with dimension 0 moved to last
+    xt, yt = transpose(x,permutx_rep_last), transpose(y,permuty_rep_last) # array transposed with rep dimension as last
+    m=prod(dim_xy,dtype=int)
+    areame_sorted_flat = empty((m,))
+    for j in range(m): # loop over all elements except the first dimension
+        start_x,end_x = int(j*rep_x), int((j+1)*rep_x)
+        start_y,end_y = int(j*rep_y), int((j+1)*rep_y)
+        x_flat = xt.flatten()[start_x:end_x]
+        y_flat = yt.flatten()[start_y:end_y]
+        areame_sorted_flat[j] = areame_algorithm(x_flat,y_flat) # here deploy area-metric algorithm
+    areame_sorted_reshape = areame_sorted_flat.reshape(dim_xy) # from flat back to shape1
+    return areame_sorted_reshape # if values are already sorted (increasing) this must return x
+
+def areaMe(x_:ndarray,y_:ndarray):
+    x, y = dataseries(x_), dataseries(y_) 
+    if is_compatible(x,y)==False: raise TypeError('The area metric can only be computed between compatible data structures.\nCompatible ds are ds with the same dimension.')
+    if x.tabular: return areame_tensor(x,y) # or y.tabular==True
+    else: return areame_algorithm(x,y)
+
+
+def areaMe_env(dataset: list) -> float:  # inputs a list of datasets of different sizes (list of lists)
+    '''
+    : --------------------------- ∞
+    cre: Oct 2020
+    edi: Mar 2020
+    aut: Marco De Angelis
+    web: github.com/marcodeangelis
+    org: University of Liverpool
+    
+    GNU General Public License v3.0
+    : --------------------------- ∞
+
+    Code for the area metric of an envelope of datasets. Currenly under testing.
+    
+    ''' 
+    def envelope(D: list): # inputs a list of datasets (list of lists)
+        F = [Dataset(d,bareclass=True).interp_f() for d in D]
+        dd =[]
+        ii =[]
+        for i,d in enumerate(D): 
+            dd+=d           # blend all data into a big dataset
+            ii+=len(d)*[i]  # assign a unique index to each element
+        dd.sort() # sort in place
+        I_min = []
+        I_max = []
+        d_prev = dd[0]
+        for d,i in zip(dd,ii):
+            Y_h = [f(d) for f in F]
+            v = (d_prev+d)/2
+            Y_l = [f(v) for f in F] # ensures interpolator on step picks the bottom value
+            ama = argmax(Y_h)
+            ami = argmin(Y_l)
+            I_max.append(ama)
+            I_min.append(ami)
+            d_prev = d
+        return I_min, I_max, F, dd
+    D = dataset.copy() # avoid catastrophic memory leaks due to the sort in place
+    minf,maxf,ff,dd = envelope(D) # -- main function starts here -- #
+    diff=[d1-d2 for d1,d2 in zip(dd[1:],dd[:-1])] # N-1 vector of differences
+    diff.sort()
+    for x in diff:
+        if x>0:
+            s=x/10 # this assigns to s the smallest difference greater than zero and makes it smaller
+            break
+    AM = 0
+    for i,_ in enumerate(dd):
+        if i<len(dd)-1:
+            d0 = dd[i] # = d
+            d1 = dd[i+1]
+            dx = d1-d0
+            dy = ff[maxf[i]](d0) - ff[minf[i+1]](d1-s)
+            AM += dx*dy
+    return AM      
+
+
+
+# def areaMe(data1: DATASET_TYPE, data2: DATASET_TYPE) -> float: # inputs lists of doubles # numpy array are not currently supported.
+#     '''
+#     ∞ --------------------------- ∞
+#     cre: Wed Oct 7 23:41 2020 
+#     edi: Wed Jun 23 11:20 2021
+#     author: Marco De Angelis 
+#     github.com/marcodeangelis 
+#     University of Liverpool 
+#     GNU General Public License v3.0 
+#     ∞ --------------------------- ∞
+#     Works also on datasets of different sizes.
+#     When datasets have same size the code is fastest.
+#     '''
+#     d1, d2 = dataset_parser(data1), dataset_parser(data2) 
+
+#     n1, n2 = len(d1), len(d2)
+#     f1_, f2_ = pseudoinverse(d1), pseudoinverse(d2)
+#     if n1==n2:
+#         n = n1 # = n2
+#         p = numpy.linspace(0,1,n)
+#         return sum(abs(f1_(p)-f2_(p))/n)
+#     else: # n1>n2
+#         # AM = 0
+#         # f1, f2 = d1.interp_f(), d2.interp_f()
+#         d12 = d1 + d2 
+#         # s12 = d12.index()
+#         v = d12.value()[d12.index()]
+#         y = abs(f1_(v)-f2_(v))
+#         x = numpy.diff(v)
+#         return x[:-1]*y[:-1]
 
 # def areaMe(data1: DATASET_TYPE, data2: DATASET_TYPE) -> float: # inputs lists of doubles # numpy array are not currently supported.
 #     '''
@@ -277,56 +418,3 @@ def areaMe(x_: ndarray, y_: ndarray) -> ndarray: # inputs lists of doubles # num
 #             x = d12[s12[i+1]]-d12[s12[i]]
 #             AM += x*y
 #     return AM
-
-def areaMe_env(dataset: list) -> float:  # inputs a list of datasets of different sizes (list of lists)
-    '''
-    ∞ --------------------------- ∞
-    Fri Oct 9 18:53:08 2020
-    @author: Marco De Angelis
-    github.com/marcodeangelis
-    University of Liverpool
-    GNU General Public License v3.0
-    ∞ --------------------------- ∞
-
-    Code for the area metric of an envelope of datasets. Currenly under testing.
-    
-    ''' 
-    def envelope(D: list): # inputs a list of datasets (list of lists)
-        F = [Dataset(d,bareclass=True).interp_f() for d in D]
-        dd =[]
-        ii =[]
-        for i,d in enumerate(D): 
-            dd+=d           # blend all data into a big dataset
-            ii+=len(d)*[i]  # assign a unique index to each element
-        dd.sort() # sort in place
-        I_min = []
-        I_max = []
-        d_prev = dd[0]
-        for d,i in zip(dd,ii):
-            Y_h = [f(d) for f in F]
-            v = (d_prev+d)/2
-            Y_l = [f(v) for f in F] # ensures interpolator on step picks the bottom value
-            ama = argmax(Y_h)
-            ami = argmin(Y_l)
-            I_max.append(ama)
-            I_min.append(ami)
-            d_prev = d
-        return I_min, I_max, F, dd
-    D = dataset.copy() # avoid catastrophic memory leaks due to the sort in place
-    minf,maxf,ff,dd = envelope(D) # -- main function starts here -- #
-    diff=[d1-d2 for d1,d2 in zip(dd[1:],dd[:-1])] # N-1 vector of differences
-    diff.sort()
-    for x in diff:
-        if x>0:
-            s=x/10 # this assigns to s the smallest difference greater than zero and makes it smaller
-            break
-    AM = 0
-    for i,_ in enumerate(dd):
-        if i<len(dd)-1:
-            d0 = dd[i] # = d
-            d1 = dd[i+1]
-            dx = d1-d0
-            dy = ff[maxf[i]](d0) - ff[minf[i+1]](d1-s)
-            AM += dx*dy
-    return AM      
-
