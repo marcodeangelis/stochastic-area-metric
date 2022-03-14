1a74c61855346f8c867c83b3a8e30e96384c3edd
Marco <marco.deangelis09@gmail.com>
2022-03-14 11:39:13 +0000

    update __init__.py

--- areametric/__init__.py
+++ areametric/__init__.py
@@ -1,5 +1,6 @@
-from .areametric import (areaMe, areaMe_env, skinny, puffy) # ParametricDataset
+from .areametric import (areaMe, areaMe_env,) 
 from .dists import (lognormal, Lognormal, normal, exponential, Exponential, uniform)
-from .plots import (plot,plot_ecdf,plot_ecdf_boxed)
-from .dataset import (Dataset,ecdf_,ecdf,pseudoinverse,pseudoinverse_,dataset_parser)
+# from .plots import (plot,plot_ecdf,plot_ecdf_boxed)
+# from .dataset import (Dataset,ecdf_,ecdf,)
+from .examples import (skinny, puffy)
 
