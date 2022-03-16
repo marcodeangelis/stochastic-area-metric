ec16204d0c11acd9321257a31d522326f6b792af
Marco <marco.deangelis09@gmail.com>
2022-03-16 17:49:33 +0000

    updated importer

--- areametric/__init__.py
+++ areametric/__init__.py
@@ -1,6 +1,5 @@
-from .areametric import (areaMe,area_chunks)
-from .dataseries import (dataseries,mixture) 
-from .methods import (ecdf, quantile_function, quantile_value, inverse_quantile_function, inverse_quantile_value)
-from .plotting import (plot_ecdf, plot_area, plot_ecdf_boxed)
+from .areametric import (areaMe,area_chunks,areame_mixture)
+from .dataseries import (dataseries,mixture,map_c_order,map_index_flat_to_array) 
+from .methods import (ecdf, ecdf_p_, ecdf_p, ecdf_p_mixture_, quantile_function, quantile_value, inverse_quantile_function, inverse_quantile_value,inverse_quantile_mixture)
+from .plotting import (plot_ecdf, plot_area, plot_ecdf_boxed, plot_mixture_area)
 from .examples import (skinny, puffy)
-
